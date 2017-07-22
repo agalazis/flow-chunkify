199a87de5c3263e56ecaff545d7742aab66fcea9
Andreas Galazis <agalazis@gmail.com>
2017-07-22 16:13:15 +0300

    added comments in examples

--- examples/index.js
+++ examples/index.js
@@ -1,7 +1,7 @@
 var eventStream = require( 'event-stream' ),
 	cStream = require( '../lib' );
 
-// Create some data...
+// Create some data for given size...
 function createArray(size){
 	var data = new Array( size );
 	for ( var i = 0; i < data.length; i++ ) {
@@ -9,21 +9,36 @@ function createArray(size){
 	}
 	return data;
 }
+
+// sets up an example given a chankify transformation
+// factory and example data size
 function setupExample(transformFactory, size){
+	
+	// create an array of given size
 	const randomArray = createArray(size);
+	
+	// create a stream out of it
 	const stream = eventStream.readArray(randomArray);
+
+	// pipe ste stream to chunkify  
 	return stream.pipe( transformFactory.stream() )
+		//improve output format
 		.pipe( eventStream.map( function( d, clbk ){
 			clbk( null, 'stream'+size+': '+JSON.stringify( d )+'\n' );
 		}))
 	
 }
 
-// Create a new chunkify stream:
+// Create a new chunkify stream factory:
 var transformFactory = cStream()
 	.numValues( 10 );
+
+// set the size of your examples here
 var exampleSizes = [5,10,11,1000]
+
+// setup examples fro all the sizes
 for (var i=0; i<exampleSizes.length; i++){
-	setupExample(transformFactory, exampleSizes[i]).pipe( process.stdout );
+	setupExample(transformFactory, exampleSizes[i])
+		// pipe the data to stdout
+		.pipe( process.stdout );
 }
-// Pipe the data:
